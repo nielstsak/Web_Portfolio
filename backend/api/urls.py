79408e6f512543e18679fda26bfbb63b36efba9d
nielstsak <niels.tsakiris1@gmail.com>
2025-12-03 11:50:03 +0100

    chore(backend): met à jour les routes API

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,32 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    PresentationViewSet, 
-    PosteCibleViewSet, 
-    DiplomeViewSet, 
+    PresentationViewSet, PosteCibleViewSet, DiplomeViewSet, 
     CompetenceTechnologiqueViewSet, SectionCompetenceViewSet,
-    FormationViewSet,
-    ActiviteProfessionnelleViewSet,
-    ServiceCiviqueViewSet,
-    ProjetProfessionnelViewSet,
-    ProjetEtudiantViewSet,
-    ProjetPersonnelViewSet,
+    FormationViewSet, ActiviteProfessionnelleViewSet, ServiceCiviqueViewSet,
+    ProjetViewSet
 )
 
 router = DefaultRouter()
-
-router.register(r'presentations', PresentationViewSet, basename='presentation')
-router.register(r'postes', PosteCibleViewSet, basename='poste')
-router.register(r'diplomes', DiplomeViewSet, basename='diplome')
-router.register(r'competences', CompetenceTechnologiqueViewSet, basename='competence')
-router.register(r'sections-competences', SectionCompetenceViewSet, basename='section-competence')
-
-router.register(r'formations', FormationViewSet, basename='formation')
-router.register(r'activites-professionnelles', ActiviteProfessionnelleViewSet, basename='activite')
-router.register(r'services-civiques', ServiceCiviqueViewSet, basename='service')
-router.register(r'projets-professionnels', ProjetProfessionnelViewSet, basename='projet-pro')
-router.register(r'projets-etudiants', ProjetEtudiantViewSet, basename='projet-etu')
-router.register(r'projets-personnels', ProjetPersonnelViewSet, basename='projet-perso')
+router.register(r'presentations', PresentationViewSet)
+router.register(r'postes', PosteCibleViewSet)
+router.register(r'diplomes', DiplomeViewSet)
+router.register(r'competences', CompetenceTechnologiqueViewSet)
+router.register(r'sections-competences', SectionCompetenceViewSet)
+router.register(r'formations', FormationViewSet)
+router.register(r'activites-professionnelles', ActiviteProfessionnelleViewSet)
+router.register(r'services-civiques', ServiceCiviqueViewSet)
+# Route unifiée
+router.register(r'projets', ProjetViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
